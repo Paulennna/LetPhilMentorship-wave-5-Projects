49f8e9c1adb0660831c6efc099453a669e493984
Paulennna <mariapaula.mpgp55@gmail.com>
2025-09-17 23:31:20 -0400

    Add files via upload

---
+++ mini-project-VariablesOnly/script.js
@@ -0,0 +1,10 @@
+const profileFirst =  "Alex";
+const profileLast = "Rivera";
+let profileAge =  28;
+let profileMember = true;
+let profileCity = "Monterrey";
+
+console.log(
+  ` ${profileFirst} ${profileLast} - Age: ${profileAge} - Member: ${profileMember} - City: ${profileCity}`
+);
+
